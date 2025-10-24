eb5666aac5e8a16ab09da84270a0584e72ab6045
LeBaptouBaptiste <baptiste.vidal.2005@gmail.com>
2025-10-24 09:37:59 +0200

    Interface created and users fetched

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { user, userList } from "@/app/types/userInterface";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Welcome to Next.js!</h1>
-    </main>
-  );
+	const [users, setUsers] = useState<userList>();
+
+	useEffect(() => {
+		fetchData().then((data) => setUsers(data));
+	}, []);
+
+	return (
+		<main>
+			<h1>Welcome to Next.js!</h1>
+			{users?.users.map((user) => (
+				<div key={user.id}>
+					<h2>
+						{user.firstName} {user.lastName}
+					</h2>
+				</div>
+			))}
+		</main>
+	);
+}
+
+async function fetchData() {
+	const data = await fetch("https://dummyjson.com/users");
+	return data.json();
 }
