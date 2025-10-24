8c13b78d8dfaf06f610b895cddb7764c777a4c8f
LeBaptouBaptiste <baptiste.vidal.2005@gmail.com>
2025-10-24 13:48:26 +0200

    Fix Error Boundary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,13 @@
 import type { Metadata } from "next";
-import { ErrorBoundary } from "react-error-boundary";
 import "./globals.css";
 import ThemeButton from "@/components/ThemeButton";
+import AppErrorBoundary from "@/components/AppErrorBoundary";
 
 export const metadata: Metadata = {
 	title: "Examen Frontend",
 	description:
 		"Application Next.js avec gestion des thèmes et liste d'utilisateurs",
 };
-
-function Fallback({ error, resetErrorBoundary }: any) {
-	return (
-		<main
-			className="min-h-screen flex flex-col items-center justify-center text-center p-6
-			bg-gray-100 text-gray-900 dark:bg-gray-950 dark:text-gray-100 transition-colors duration-300"
-		>
-			<h1 className="text-2xl font-bold text-fuchsia-500 mb-4">
-				Une erreur est survenue
-			</h1>
-			<p className="text-gray-400 mb-6">{error.message}</p>
-			<button
-				onClick={resetErrorBoundary}
-				className="px-4 py-2 bg-fuchsia-600 hover:bg-fuchsia-700 text-white rounded-lg font-medium transition-all"
-			>
-				Recharger
-			</button>
-		</main>
-	);
-}
 
 export default function RootLayout({
 	children,
@@ -36,14 +16,11 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="fr">
-			<body className="transition-colors duration-300">
-				<ErrorBoundary
-					FallbackComponent={Fallback}
-					onReset={() => window.location.reload()}
-				>
+			<body>
+				<AppErrorBoundary>
 					{children}
 					<ThemeButton />
-				</ErrorBoundary>
+				</AppErrorBoundary>
 			</body>
 		</html>
 	);
